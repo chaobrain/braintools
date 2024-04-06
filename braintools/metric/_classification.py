cd82d47836865c8ace00020bbbce8a3d0a07e833
Chaoming Wang <adaduo@outlook.com>
2024-04-06 15:55:36 +0800

    more loss functions

--- braintools/metric/_classification.py
+++ braintools/metric/_classification.py
@@ -1,7 +1,7 @@
 # This file is modified from [optax/losses](https://github.com/google-deepmind/optax).
 # The copyright notice is as follows:
 #
-# Copyright 2019 DeepMind Technologies Limited. All Rights Reserved.
+# Copyright 2024 BrainPy Ecosystem Limited. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- braintools/metric/_regression.py
+++ braintools/metric/_regression.py
@@ -20,9 +20,14 @@ from typing import Optional, Union
 import braincore as bc
 import jax.numpy as jnp
 
+from ._util import _reduce
+
 __all__ = [
   'squared_error',
+  'absolute_error',
+  'l1_loss',
   'l2_loss',
+  'l2_norm',
   'huber_loss',
   'log_cosh',
   'cosine_similarity',
@@ -69,10 +74,12 @@ def safe_norm(x: bc.typing.ArrayLike,
 def squared_error(
     predictions: bc.typing.ArrayLike,
     targets: Optional[bc.typing.ArrayLike] = None,
+    axis: Optional[Union[int, tuple[int, ...]]] = None,
+    reduction: str = 'mean',
 ) -> bc.typing.ArrayLike:
   """Calculates the squared error for a set of predictions.
 
-  Mean Squared Error can be computed as squared_error(a, b).mean().
+  Mean Squared Error can be computed as ``squared_error(a, b, reduction='mean')``.
 
   Note: l2_loss = 0.5 * squared_error, where the 0.5 term is standard in
   "Pattern Recognition and Machine Learning" by Bishop, but not
@@ -85,6 +92,9 @@ def squared_error(
     predictions: a vector of arbitrary shape `[...]`.
     targets: a vector with shape broadcastable to that of `predictions`;
       if not provided then it is assumed to be a vector of zeros.
+    axis: the dimensions to reduce. If `None`, the loss is reduced to a scalar.
+    reduction: the reduction operation to apply to the output. One of
+      'none', 'mean', 'sum'. Default is 'mean'.
 
   Returns:
     elementwise squared differences, with same shape as `predictions`.
@@ -94,7 +104,152 @@ def squared_error(
     # Avoid broadcasting logic for "-" operator.
     assert predictions.shape == targets.shape, 'predictions and targets must have the same shape.'
   errors = predictions - targets if targets is not None else predictions
-  return errors ** 2
+  return _reduce(errors ** 2, reduction, axis=axis)
+
+
+def absolute_error(
+    predictions: bc.typing.ArrayLike,
+    targets: Optional[bc.typing.ArrayLike] = None,
+    axis: Optional[Union[int, tuple[int, ...]]] = None,
+    reduction: str = 'mean',
+) -> bc.typing.ArrayLike:
+  """Calculates the absolute error for a set of predictions.
+
+  Mean Absolute Error can be computed as absolute_error(a, b).mean().
+
+  Args:
+    predictions: a vector of arbitrary shape `[...]`.
+    targets: a vector with shape broadcastable to that of `predictions`;
+      if not provided then it is assumed to be a vector of zeros.
+    axis: the dimensions to reduce. If `None`, the loss is reduced to a scalar.
+    reduction: the reduction operation to apply to the output. One of
+      'none', 'mean', 'sum'. Default is 'mean'.
+
+  Returns:
+    elementwise absolute differences, with same shape as `predictions`.
+  """
+  assert bc.math.is_float(predictions), 'predictions must be float.'
+  if targets is not None:
+    # Avoid broadcasting logic for "-" operator.
+    assert predictions.shape == targets.shape, 'predictions and targets must have the same shape.'
+  errors = predictions - targets if targets is not None else predictions
+  return _reduce(jnp.abs(errors), reduction, axis=axis)
+
+
+class L1Loss:
+  r"""Creates a criterion that measures the mean absolute error (MAE) between each element in
+  the input :math:`x` and target :math:`y`.
+
+  The unreduced (i.e. with :attr:`reduction` set to ``'none'``) loss can be described as:
+
+  .. math::
+      \ell(x, y) = L = \{l_1,\dots,l_N\}^\top, \quad
+      l_n = \left| x_n - y_n \right|,
+
+  where :math:`N` is the batch size. If :attr:`reduction` is not ``'none'``
+  (default ``'mean'``), then:
+
+  .. math::
+      \ell(x, y) =
+      \begin{cases}
+          \operatorname{mean}(L), & \text{if reduction} = \text{`mean';}\\
+          \operatorname{sum}(L),  & \text{if reduction} = \text{`sum'.}
+      \end{cases}
+
+  :math:`x` and :math:`y` are tensors of arbitrary shapes with a total
+  of :math:`n` elements each.
+
+  The sum operation still operates over all the elements, and divides by :math:`n`.
+
+  The division by :math:`n` can be avoided if one sets ``reduction = 'sum'``.
+
+  Supports real-valued and complex-valued inputs.
+
+  Args:
+      reduction (str, optional): Specifies the reduction to apply to the output:
+          ``'none'`` | ``'mean'`` | ``'sum'``. ``'none'``: no reduction will be applied,
+          ``'mean'``: the sum of the output will be divided by the number of
+          elements in the output, ``'sum'``: the output will be summed. Note: :attr:`size_average`
+          and :attr:`reduce` are in the process of being deprecated, and in the meantime,
+          specifying either of those two args will override :attr:`reduction`. Default: ``'mean'``
+
+  Shape:
+      - Input: :math:`(*)`, where :math:`*` means any number of dimensions.
+      - Target: :math:`(*)`, same shape as the input.
+      - Output: scalar. If :attr:`reduction` is ``'none'``, then
+        :math:`(*)`, same shape as the input.
+
+  Examples::
+
+      >>> loss = nn.L1Loss()
+      >>> input = bm.random.randn(3, 5)
+      >>> target = bm.random.randn(3, 5)
+      >>> output = loss(input, target)
+      >>> output.backward()
+  """
+
+  def __init__(self, reduction: str = 'mean') -> None:
+    self.reduction = reduction
+
+  def update(self,
+             input: bc.typing.ArrayLike,
+             target: bc.typing.ArrayLike) -> bc.typing.ArrayLike:
+    return l1_loss(input, target, reduction=self.reduction)
+
+
+def l1_loss(logits: bc.typing.ArrayLike,
+            targets: bc.typing.ArrayLike,
+            reduction: str ='sum'):
+  r"""Creates a criterion that measures the mean absolute error (MAE) between each element in
+  the logits :math:`x` and targets :math:`y`. It is useful in regression problems.
+
+  The unreduced (i.e. with :attr:`reduction` set to ``'none'``) loss can be described as:
+
+  .. math::
+      \ell(x, y) = L = \{l_1,\dots,l_N\}^\top, \quad
+      l_n = \left| x_n - y_n \right|,
+
+  where :math:`N` is the batch size. If :attr:`reduction` is not ``'none'``
+  (default ``'mean'``), then:
+
+  .. math::
+      \ell(x, y) =
+      \begin{cases}
+          \operatorname{mean}(L), & \text{if reduction} = \text{`mean';}\\
+          \operatorname{sum}(L),  & \text{if reduction} = \text{`sum'.}
+      \end{cases}
+
+  :math:`x` and :math:`y` are tensors of arbitrary shapes with a total
+  of :math:`n` elements each.
+
+  The sum operation still operates over all the elements, and divides by :math:`n`.
+
+  The division by :math:`n` can be avoided if one sets ``reduction = 'sum'``.
+
+  Supports real-valued and complex-valued inputs.
+
+  Parameters
+  ----------
+  logits : bc.typing.ArrayLike
+    :math:`(N, *)` where :math:`*` means, any number of additional dimensions.
+  targets : bc.typing.ArrayLike
+    :math:`(N, *)`, same shape as the input.
+  reduction : str
+    Specifies the reduction to apply to the output: ``'none'`` | ``'mean'`` | ``'sum'``.
+    Default: ``'mean'``.
+    - ``'none'``: no reduction will be applied,
+    - ``'mean'``: the sum of the output will be divided by the number of elements in the output,
+    - ``'sum'``: the output will be summed. Note: :attr:`size_average`
+
+  Returns
+  -------
+  output : scalar.
+    If :attr:`reduction` is ``'none'``, then :math:`(N, *)`, same shape as the input.
+  """
+
+  diff = (logits - targets).reshape((logits.shape[0], -1))
+  norm = jnp.linalg.norm(diff, ord=1, axis=1, keepdims=False)
+  return _reduce(outputs=norm, reduction=reduction)
 
 
 def l2_loss(
@@ -118,6 +273,30 @@ def l2_loss(
     elementwise squared differences, with same shape as `predictions`.
   """
   return 0.5 * squared_error(predictions, targets)
+
+
+def l2_norm(
+    predictions: bc.typing.ArrayLike,
+    targets: Optional[bc.typing.ArrayLike] = None,
+    axis: Optional[Union[int, tuple[int, ...]]] = None,
+) -> bc.typing.ArrayLike:
+  """Computes the L2 norm of the difference between predictions and targets.
+
+  Args:
+    predictions: a vector of arbitrary shape `[...]`.
+    targets: a vector with shape broadcastable to that of `predictions`;
+      if not provided then it is assumed to be a vector of zeros.
+    axis: the dimensions to reduce. If `None`, the loss is reduced to a scalar.
+
+  Returns:
+    elementwise l2 norm of the differences, with same shape as `predictions`.
+  """
+  assert bc.math.is_float(predictions), 'predictions must be float.'
+  if targets is not None:
+    # Avoid broadcasting logic for "-" operator.
+    assert predictions.shape == targets.shape, 'predictions and targets must have the same shape.'
+  errors = predictions - targets if targets is not None else predictions
+  return jnp.linalg.norm(errors, axis=axis, ord=2)
 
 
 def huber_loss(
@@ -207,9 +386,9 @@ def cosine_similarity(
   batched_norm_fn = jnp.vectorize(safe_norm, signature='(k)->()', excluded={1})
   # normalise the last dimension of targets and predictions.
   unit_targets = targets / jnp.expand_dims(
-      batched_norm_fn(targets, epsilon), axis=-1)
+    batched_norm_fn(targets, epsilon), axis=-1)
   unit_predictions = predictions / jnp.expand_dims(
-      batched_norm_fn(predictions, epsilon), axis=-1)
+    batched_norm_fn(predictions, epsilon), axis=-1)
   # return cosine similarity.
   return jnp.sum(unit_targets * unit_predictions, axis=-1)
 
@@ -239,3 +418,7 @@ def cosine_distance(
   assert bc.math.is_float(targets), 'targets must be float.'
   # cosine distance = 1 - cosine similarity.
   return 1. - cosine_similarity(predictions, targets, epsilon=epsilon)
+
+
+
+
