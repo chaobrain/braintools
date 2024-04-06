cac7175e0246b1a7a92e7443bc4dcc662df7c72b
Chaoming Wang <adaduo@outlook.com>
2024-04-06 10:13:38 +0800

    renaming

--- braintools/functional/__init__.py
+++ braintools/functional/__init__.py
@@ -1,8 +1,8 @@
 
 from ._activations import *
 from ._activations import __all__ as __activations_all__
-from ._others import *
-from ._others import __all__ as __others_all__
+from .normalization import *
+from .normalization import __all__ as __others_all__
 from ._spikes import *
 from ._spikes import __all__ as __spikes_all__
 
