f9741698f67876eb3db1526da2aae8727e612caa
chaoming <adaduo@outlook.com>
2025-05-08 11:47:00 +0800

    refactor: improve indentation and formatting in setup.py

--- setup.py
+++ setup.py
@@ -27,79 +27,80 @@ from setuptools import setup
 # version
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'braintools/', '__init__.py'), 'r') as f:
-  init_py = f.read()
+    init_py = f.read()
 version = re.search('__version__ = "(.*)"', init_py).groups()[0]
 if len(sys.argv) > 2 and sys.argv[2] == '--python-tag=py3':
-  version = version
+    version = version
 else:
-  version += '.post{}'.format(time.strftime("%Y%m%d", time.localtime()))
+    version += '.post{}'.format(time.strftime("%Y%m%d", time.localtime()))
 
 # obtain long description from README
 with io.open(os.path.join(here, 'README.md'), 'r', encoding='utf-8') as f:
-  README = f.read()
+    README = f.read()
 
 # installation packages
 packages = find_packages(
-  exclude=[
-    "docs*", "tests*", "examples*", "build*",
-    "dist*", "braintools.egg-info*", "braintools/__pycache__*"
-  ]
+    exclude=[
+        "docs*",
+        "tests*",
+        "examples*",
+        "build*",
+        "dist*",
+        "braintools.egg-info*",
+        "braintools/__pycache__*"
+    ]
 )
 
 all_dependencies = ['scienceplots', 'matplotlib', 'nevergrad', 'scipy', 'msgpack']
 
 # setup
 setup(
-  name='braintools',
-  version=version,
-  description='Commonly Used Toolboxes for Brain Dynamics Programming',
-  long_description=README,
-  long_description_content_type="text/markdown",
-  author='BDP',
-  author_email='chao.brain@qq.com',
-  packages=packages,
-  python_requires='>=3.10',
-  install_requires=[
-    'numpy>=1.15',
-    'jax',
-    'brainstate>=0.1.0',
-    'brainunit>=0.0.8',
-    'typing_extensions',
-  ],
-  url='https://github.com/chaobrain/braintools',
-  project_urls={
-    "Bug Tracker": "https://github.com/chaobrain/braintools/issues",
-    "Documentation": "https://braintools.readthedocs.io/",
-    "Source Code": "https://github.com/chaobrain/braintools",
-  },
-  extras_require={
-    'cpu': ['jaxlib'],
-    'cuda12': ['jaxlib[cuda12]'],
-    'tpu': ['jaxlib[tpu]'],
-    'cuda_all': ['jaxlib[cuda12]'] + all_dependencies,
-    'cpu_all': ['jaxlib'] + all_dependencies,
-    'tpu_all': ['jaxlib[tpu]'] + all_dependencies,
-  },
-  keywords=(
-    'computational neuroscience, '
-    'brain-inspired computation, '
-    'brain dynamics programming'
-  ),
-  classifiers=[
-    'Natural Language :: English',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.10',
-    'Programming Language :: Python :: 3.11',
-    'Programming Language :: Python :: 3.12',
-    'Programming Language :: Python :: 3.13',
-    'Intended Audience :: Science/Research',
-    'License :: OSI Approved :: Apache Software License',
-    'Topic :: Scientific/Engineering :: Bio-Informatics',
-    'Topic :: Scientific/Engineering :: Mathematics',
-    'Topic :: Scientific/Engineering :: Artificial Intelligence',
-    'Topic :: Software Development :: Libraries',
-  ],
-  license='Apache-2.0 license',
+    name='braintools',
+    version=version,
+    description='Commonly Used Toolboxes for Brain Dynamics Programming',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author='BDP',
+    author_email='chao.brain@qq.com',
+    packages=packages,
+    python_requires='>=3.10',
+    install_requires=[
+        'numpy>=1.15',
+        'brainstate>=0.1.0',
+        'brainunit>=0.0.8',
+        'typing_extensions',
+    ],
+    url='https://github.com/chaobrain/braintools',
+    project_urls={
+        "Bug Tracker": "https://github.com/chaobrain/braintools/issues",
+        "Documentation": "https://braintools.readthedocs.io/",
+        "Source Code": "https://github.com/chaobrain/braintools",
+    },
+    extras_require={
+        'cpu': ['jax'] + all_dependencies,
+        'cuda12': ['jax[cuda12]'] + all_dependencies,
+        'tpu': ['jax[tpu]'] + all_dependencies,
+    },
+    keywords=(
+        'computational neuroscience, '
+        'brain-inspired computation, '
+        'brain dynamics programming'
+    ),
+    classifiers=[
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development :: Libraries',
+    ],
+    license='Apache-2.0 license',
 )
