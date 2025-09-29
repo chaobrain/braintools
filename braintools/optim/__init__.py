4de8c31fa2e777692891307210be9ca766fd27f0
Chaoming Wang <adaduo@outlook.com>
2025-09-29 22:12:27 +0800

    Rename lr_scheduler files to optax_lr_scheduler for improved clarity and consistency

--- braintools/optim/__init__.py
+++ braintools/optim/__init__.py
@@ -16,8 +16,8 @@
 
 from ._base import *
 from ._base import __all__ as base_all
-from ._lr_scheduler import *
-from ._lr_scheduler import __all__ as lr_scheduler_all
+from ._optax_lr_scheduler import *
+from ._optax_lr_scheduler import __all__ as lr_scheduler_all
 from ._nevergrad_optimizer import *
 from ._nevergrad_optimizer import __all__ as ng_optim_all
 from ._optax_optimizer import *
