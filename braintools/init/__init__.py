24f46cf1c0fd76ed7915e839ccd3243bc0b7c247
oujago <oujago@gmail.com>
2025-10-01 14:16:13 +0800

    Add variance scaling and orthogonal weight initialization strategies

--- braintools/init/__init__.py
+++ braintools/init/__init__.py
@@ -24,6 +24,10 @@ from braintools.init._init_basic import *
 from braintools.init._init_basic import __all__ as _init_basic_all
 from braintools.init._init_composite import *
 from braintools.init._init_composite import __all__ as composite_all
+from braintools.init._init_variance_scaling import *
+from braintools.init._init_variance_scaling import __all__ as variance_scaling_all
+from braintools.init._init_orthogonal import *
+from braintools.init._init_orthogonal import __all__ as orthogonal_all
 
-__all__ = init_all + _init_basic_all + distance_all + distance_impl_all + composite_all
-del init_all, _init_basic_all, distance_all, distance_impl_all, composite_all
+__all__ = init_all + _init_basic_all + distance_all + distance_impl_all + composite_all + variance_scaling_all + orthogonal_all
+del init_all, _init_basic_all, distance_all, distance_impl_all, composite_all, variance_scaling_all, orthogonal_all
