1dcc6e520371491a5291f78300cb7a46d5a0e5f4
Chaoming Wang <adaduo@outlook.com>
2026-01-23 13:20:37 +0800

    Bump version to 0.1.8

--- braintools/__init__.py
+++ braintools/__init__.py
@@ -14,7 +14,7 @@
 # ==============================================================================
 
 
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 __version_info__ = tuple(map(int, __version__.split(".")))
 
 from . import conn
